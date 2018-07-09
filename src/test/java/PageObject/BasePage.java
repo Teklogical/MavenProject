af1961e073ce987e3e2dd8d123de3871bc254274
sardar yalkun <sardaryalkun1023@gmail.com>
2018-07-08 21:45:15 -0600

    modified Base page

--- src/test/java/PageObject/BasePage.java
+++ src/test/java/PageObject/BasePage.java
@@ -1,27 +1,13 @@
 package PageObject;
 
-<<<<<<< HEAD
 import org.openqa.selenium.WebDriver;
 
-
-
 public class BasePage {
-static WebDriver driver;
-	
+	static WebDriver driver;
+
 	public BasePage(WebDriver driver) {
 		BasePage.driver = driver;
-		
-		
-	}
-
 
 	}
-
-
-
-
-=======
-public class BasePage {
 
 }
->>>>>>> refs/heads/Team-B
